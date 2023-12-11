0badd39006e6c7b4947013e62704a82a846300b1
Matt Smida <mcs@m4t7.com>
2023-12-11 15:11:48 -0500

    made models, set up the initial db connections in app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 """Flask app for Cupcakes"""
+
+import os
+from flask import Flask
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
